b1e0256f713135a0d60ec89e2d67beed0b1e1529
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-05-20 14:49:53 +0400

    added item amount counter

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef, useState } from "react";
+
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
-const MealItem = (props) => {
+const MealItemForm = (props) => {
+  const [amountIsvalid, setAmountIsvalid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsvalid(false);
+      return;
+    } 
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id, //this changed!
@@ -16,8 +40,9 @@ const MealItem = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsvalid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
 
-export default MealItem;
+export default MealItemForm;
